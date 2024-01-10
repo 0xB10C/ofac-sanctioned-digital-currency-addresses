d95712febb118fb5b258af3c9c8fe17a4eeae9d1
Gonchik Tsymzhitov <gonchik.tsymzhitov@atlassiancommunity.com>
2024-01-10 18:08:14 +0200

    Just add extra prints

--- runner.sh
+++ runner.sh
@@ -5,3 +5,6 @@ curl -Lo sdn_advanced.xml https://www.treasury.gov/ofac/downloads/sanctions/1.0/
 
 echo "Generating CSV file of aggregated addresses"
 python3 generate-address-list.py -a -f CSV
+
+echo "Successfully generated CSV file of aggregated addresses"
+echo "Enjoy your day!"
